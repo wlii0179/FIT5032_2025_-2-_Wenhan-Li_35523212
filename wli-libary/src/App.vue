975a4dca67ca74dd92183981f8c5bd412dc4349b
wlii0179 <wlii0179@student.monash.edu>
2025-07-08 15:47:10 +0800

    Lab3

--- wli-libary/src/App.vue
+++ wli-libary/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
 //import HelloWorld from './components/HelloWorld.vue'
 //import TheWelcome from './components/TheWelcome.vue'
-import JSON from './components/JSON.vue'
+//import JSON from './components/JSON.vue'
+import JSON from './components/Form.vue'
 </script>
 
 <template>
